a81fa0cf92ec1b350b2c35e5b09a9b07e34093a9
Pavel-Pro <proskpavel@gmail.com>
2022-06-01 19:42:09 +0300

    destop

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	// $('.burger').click(function() {
+	// 	$('.menu-right').toggleClass('active-menu')
+	// 	$('.menu-left').toggleClass('active-menu');
+	// 	$(this).toggleClass('open');
+		// $('.burger__item:first-child').toggleClass('burger__item-top')
+		// $('.burger__item:nth-child(2)').toggleClass('burger__item-middle')
+		// $('.burger__item:last-child').toggleClass('burger__item-bottom');
+	// });
+	// $('.testimonials__slider').slick({
+	//     slidesToShow: 1,
+	//     dots: true    
+	// });
+	$('.main__sonnik').animate({left: "0px" },1200,"linear");
+});
+
