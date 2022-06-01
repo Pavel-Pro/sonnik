a5bc296182b4b21b4f01a338f256d257364e5b98
Pavel-Pro <proskpavel@gmail.com>
2022-06-01 20:06:24 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,6 +11,6 @@ $(document).ready(function() {
 	//     slidesToShow: 1,
 	//     dots: true    
 	// });
-	$('.main__sonnik').animate({left: "0px" },1200,"linear");
+	$('.main__sonnik').delay(1500).animate({left: "0px" },1200,"linear");
 });
 
