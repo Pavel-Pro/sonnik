c2b0c6164595b74a3a64b1c6903067c7ef066445
Pavel-Pro <proskpavel@gmail.com>
2022-06-21 13:33:58 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
 $(document).ready(function() {
-	// $('.burger').click(function() {
-	// 	$('.menu-right').toggleClass('active-menu')
-	// 	$('.menu-left').toggleClass('active-menu');
-	// 	$(this).toggleClass('open');
-		// $('.burger__item:first-child').toggleClass('burger__item-top')
-		// $('.burger__item:nth-child(2)').toggleClass('burger__item-middle')
-		// $('.burger__item:last-child').toggleClass('burger__item-bottom');
-	// });
-	// $('.testimonials__slider').slick({
-	//     slidesToShow: 1,
-	//     dots: true    
-	// });
+	$('.burger').click(function() {
+		$('.menu').slideToggle('500');
+		$('.header').toggleClass('header__bg', 500);
+		$('.logo__inner').toggleClass('display-none');
+		$('.social').toggleClass('display-block');
+		$(this).toggleClass('open');
+	});
 	$('.main__sonnik').delay(800).animate({left: "0px" },1200,"linear");
 	new WOW().init();
 });
